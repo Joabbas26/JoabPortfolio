4d24e81db741aa4748895f2bddfb27d5732d722a
Joab <joabbastidas@gmail.com>
2023-09-01 21:30:59 -0400

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css'
 // Need to import componenets for React Router
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
+import Projects from './components/Projects'
+import Pokedex from './components/Pokedex'
+import Weather from './components/Weather'
+import Ranking from './components/Ranking'
 
 function App() {
 
