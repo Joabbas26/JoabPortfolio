f3c23c530b8971c10b3044f931ffc21682044328
Joab <joabbastidas@gmail.com>
2023-09-22 20:19:47 -0400

    Added Pokedex asset

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,10 @@
 import './Projects.scss'
+import Pokedex from './assets/Pokedex.png'
 
 export default function Projects() {
   return (
     <div>
-        
+        <image src={Pokedex}></image>
     </div>
   )
 }
