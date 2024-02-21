c7378063f214e67ffba378e993788b01ba12f2a7
Joab <joabbastidas@gmail.com>
2024-02-21 16:44:31 -0500

    Finished setting up Pokedex app, bug fixes

--- src/components/PokedexApp.jsx
+++ src/components/PokedexApp.jsx
@@ -1,77 +1,124 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStop, faPlay, faShuffle } from '@fortawesome/free-solid-svg-icons';
 
 export default function PokedexApp() {
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonData, setPokemonData] = useState(null);
-  const [pokemonEntry, setPokemonEntry] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [pokemonDescription, setPokemonDescription] = useState("");
 
-  const handleInputChange = (event) => {
-    setPokemonName(event.target.value);
+
+  const searchPokemon = async () => {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+      );
+      const data = response.data;
+      setPokemonData(data);
+  
+      // Fetch pokemon description
+      const speciesResponse = await axios.get(data.species.url);
+      const speciesData = speciesResponse.data;
+      const description = speciesData.flavor_text_entries.find(
+        entry => entry.language.name === 'en'
+      );
+      setPokemonDescription(description.flavor_text);
+    } catch (error) {
+      console.error(error);
+      setPokemonData(null);
+      setPokemonDescription("");
+    }
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault()
-    setLoading(true);
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
-      .then((response) => {
-        setPokemonData(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setPokemonData(null);
-        setLoading(false);
-      });
-      
-      // Api for pokedex entries
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName.toLowerCase()}`)
-      .then((response) => {
-        setPokemonEntry(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setPokemonEntry(null);
-        setLoading(false);
-      })
+  const fetchRandomPokemon = async () => {
+    try {
+      const randomId = Math.floor(Math.random() * 898) + 1; // There are currently 898 Pokémon
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+      setPokemonData(response.data);
+    } catch (error) {
+      console.error('Error fetching random Pokémon:', error);
+    }
   };
 
   return (
-    <div className='container'>
-      <div className="pokedexTitle"><h1>Pokédex</h1></div>
-      <div className="searchDiv">
-      <input
-        className="pokedexInput"
-        type="text"
-        value={pokemonName}
-        onChange={handleInputChange}
-        placeholder="Enter Pokémon name"
-        onKeyDown={e => e.key === 'Enter' && handleSearch(e)}
-      />
-      <button className="pokedexSearchButton" onClick={handleSearch} disabled={loading}>
-        {loading ? 'Searching...' : 'Search'}
-      </button>
+  <div className='justify-center items-center py-20 bg-gray-800'>
+      <div className="flex items-center justify-center my-5">
+        <input
+          className="border border-gray-400 p-2 mr-2 text-gray-800"
+          type="text"
+          value={pokemonName}
+          onChange={(e) => setPokemonName(e.target.value)}
+          placeholder="Enter Pokémon name"
+          onKeyDown={e => e.key === 'Enter' && searchPokemon(e)}
+        />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={searchPokemon} disabled={loading}>
+          {loading ? 'Searching...' : 'Search'}
+        </button>
       </div>
-      <div className="container" id='pokedexData'>
-      {pokemonData && (
-        <div>
-          <h2>{pokemonData.name[0].toUpperCase() + pokemonData.name.substring(1)}</h2>
-          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
-          <h3>Types:</h3>
-          <ul>
-            {pokemonData.types.map((type) => (
-              <p key={type.slot}>{type.type.name}</p>
-            ))}
-          </ul>
-          <h3>{pokemonData.id}</h3>
-          <p>{pokemonEntry.flavor_text_entries}</p>
+
+    {pokemonData && (
+    <div className="flex justify-center items-center my-12">
+      <div className="grid grid-cols-3 grid-rows-3 w-96 h-auto bg-red-900 border border-black rounded-xl">
+        {/* Dark blue circle on the top left corner */}
+        <div className="row-start-1 col-start-1 m-1 flex justify-center items-center">
+          <div className="w-40 h-20 m-5 bg-gray-800 rounded-full">
+          {pokemonData.sprites && (
+        <img
+            src={pokemonData.sprites.front_default}
+            alt={pokemonData.name}
+            className="w-48 h-24"
+          /> )}
+          </div>
         </div>
-      )}
+        
+        {/* Div on the top right corner with 4 rows */}
+        <div className="row-start-1 col-start-3 flex flex-col justify-between">
+          <div className=" p-4 flex flex-col justify-end">
+            <div className="flex justify-end items-center w-full">
+            <div className="w-12 h-3 rounded-full bg-blue-600 mx-1"></div>
+            <div className="w-12 h-3 rounded-full bg-yellow-300 mx-1"></div>
+            <div className="w-12 h-3 rounded-full bg-green-600 mx-1"></div>
+            <div className="w-36 h-8 rounded-full bg-black mx-1"></div>
+          </div>
+          <div className="flex justify-end">
+            <h2 className="text-2xl font-bold">{pokemonData.name}</h2>
+          </div>
+          <div className="flex justify-end">
+            <h2>#{pokemonData.id}</h2>
+          </div>
+          <div className='flex justify-end'>
+            {pokemonData.types && pokemonData.types.length > 0 && (
+            <p className="text-lg font-semibold mb-2">
+            {pokemonData.types.map((type) => type.type.name).join(", ")}
+            </p>
+            )}
+        </div>
+          </div>
+        </div>
+        
+        {/* Big text box in the center */}
+        <div className="row-start-2 col-start-1 col-end-4 mx-8 flex justify-center items-center h-auto">
+          <p className="bg-white border border-black rounded p-4 text-gray-800">
+            {pokemonDescription}
+          </p>
+        </div>
+        
+        {/* Row of 3 buttons on the bottom */}
+        <div className="row-start-3 col-start-1 col-end-4 flex justify-center items-center">
+          <button className="border border-black p-6 mx-2">
+            <FontAwesomeIcon icon={faStop} />
+          </button>
+          <button className="border border-black p-6 mx-2">
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+          <button className="border border-black p-6 mx-2" onClick={fetchRandomPokemon}>
+            <FontAwesomeIcon icon={faShuffle} />
+          </button>
+        </div>
       </div>
     </div>
-  );
-};
+     )}
+ </div>
+);};
